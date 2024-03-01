d9e016e683bf053a9ea740f17dbb29f6ce899326
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-01 00:25:35 -0300

    feat: adicionando animações de fade-in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,9 @@ import { OpenNavIcon } from "../OpenNavIcon";
 import MenuNavegation from "../MenuNavegation/MenuNavegation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+//animação
+import { motion } from "framer-motion";
+
 const Header = () => {
   //gerencia o estado do menu: ativo(true) ou desativado(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,17 +38,36 @@ const Header = () => {
       </div>
       <div id="content-container">
         <div id="text-container">
-          <h1>Desenvolvedor Front-end</h1>
-          <div id="infos-container">
+          <motion.h1
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            Desenvolvedor Front-end
+          </motion.h1>
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+            id="infos-container"
+          >
             <p>
               Olá devs e recrutadores! Meu nome é Vitor Cesar Kravszenko,
               estudante de Desenvolvimento Web e Tecnologia, com 20 anos de
               idade, atualmente concentrado em aprimorar minhas habilidades
               enquanto estudo Engenharia de Software.
             </p>
-          </div>
+          </motion.div>
         </div>
-        <div id="img-perfil-container">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          id="img-perfil-container"
+        >
           <Image
             src="/perfil.jpg"
             width={220}
@@ -53,7 +75,7 @@ const Header = () => {
             alt="Foto de perfil"
             className="img-perfil"
           />
-        </div>
+        </motion.div>
       </div>
     </header>
   );
