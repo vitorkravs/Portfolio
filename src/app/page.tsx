c5439a309756f3a6f5b6ff738afa5ca328976391
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-02 19:18:02 -0300

    feat: criando o componente AboutMe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import AboutMe from "@/components/AboutMe/AboutMe";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main>
+        <AboutMe />
+      </main>
     </>
   );
 }
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const AboutMe = () => {
+  return (
+    <div id="about-me-container">
+      <h2>Sobre mim</h2>
+    </div>
+  );
+};
+
+export default AboutMe;
