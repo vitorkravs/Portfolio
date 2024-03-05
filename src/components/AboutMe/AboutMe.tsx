2a40d51baf1ef94de5382ad143e76fb882fe6959
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-04 23:21:23 -0300

    feat: adicionando animação ao componente

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 //styles
 import SlideAboutMe from "./SlideAboutMe";
 import "./styles.scss";
 
+//animação
+import { motion } from "framer-motion";
+
 const AboutMe = () => {
   return (
-    <div id="about-me-container">
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5, delay: 0.5 }}
+      id="about-me-container"
+    >
       <div id="title-about-me">
         <h2 id="title">Sobre mim</h2>
         <span className="border-title"></span>
       </div>
 
       <SlideAboutMe />
-    </div>
+    </motion.div>
   );
 };
 
