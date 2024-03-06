8cdaefe359657b2b222df322b833554c51504fc1
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-06 18:22:07 -0300

    feat: adicionado componente Project a page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "@/components/AboutMe/AboutMe";
 import Header from "@/components/Header/Header";
+import Projects from "@/components/Projects/Projects";
 import { register } from "swiper/element/bundle";
 register();
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <main>
         <AboutMe />
+        <Projects />
       </main>
     </>
   );
