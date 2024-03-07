2e00b3b4b1df29254b6545893ec748dad4644c6a
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-07 18:28:55 -0300

    feat: criando a seção de conhecimentos

--- src/components/Knowledge/Knowledge.tsx
+++ src/components/Knowledge/Knowledge.tsx
@@ -1,10 +1,73 @@
-import TitleContainer from "../TitleContainer/TitleContainer";
+"use client";
+
+//styles
 import "./styles.scss";
 
+//components
+import TitleContainer from "../TitleContainer/TitleContainer";
+
+//icons
+import {
+  iconsInThisProject,
+  iconsKnowledgeContainer,
+  iconsKnowledgeContainerTwo,
+} from "./KnowledgeIcons";
+import { SlArrowDownCircle, SlArrowUpCircle } from "react-icons/sl";
+
+//hooks
+import { useState } from "react";
+
 const Knowledge = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  //altera o estado
+  const toggleDisplay = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <section id="conhecimentos-container">
-      <TitleContainer text="Conhecimentos" />
+    <section id="knowledge-container">
+      <TitleContainer text="Hard Skills" />
+      <div id="knowledge">
+        <div id="in-this-project">
+          <h3>Usados nesse Portfólio:</h3>
+          <div id="icons-in-this-project">
+            {iconsInThisProject.map(({ id, Icon, skill }) => (
+              <p key={id}>
+                <Icon className="icons-knowledge" /> {skill}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div id="general-knowledge">
+          <h3>Conhecimentos Gerais:</h3>
+
+          <div id="icons-knowledge-container">
+            {iconsKnowledgeContainer.map(({ id, Icon, skill }) => (
+              <p key={id}>
+                <Icon className="icons-knowledge" /> {skill}
+              </p>
+            ))}
+
+            {iconsKnowledgeContainerTwo.map(({ id, Icon, skill }) => (
+              <p
+                key={id}
+                className={
+                  isActive
+                    ? "active-icons-knowledge"
+                    : "desable-icons-knowledge"
+                }
+              >
+                <Icon className="icons-knowledge" /> {skill}
+              </p>
+            ))}
+          </div>
+          <button className="btn-knowledge" onClick={toggleDisplay}>
+            {isActive ? "Ver menos" : "Veja mais"}
+            {isActive ? <SlArrowUpCircle /> : <SlArrowDownCircle />}
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/components/Knowledge/KnowledgeIcons.ts
@@ -0,0 +1,54 @@
+import {
+  SiAmazonaws,
+  SiBootstrap,
+  SiCss3,
+  SiExpress,
+  SiGit,
+  SiGithub,
+  SiHtml5,
+  SiJavascript,
+  SiMongodb,
+  SiMysql,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiSass,
+  SiStyledcomponents,
+  SiSwiper,
+  SiTailwindcss,
+  SiTypescript,
+  SiVercel,
+} from "react-icons/si";
+
+export const iconsInThisProject = [
+  { id: 1, Icon: SiNextdotjs, skill: "NextJs" },
+  { id: 2, Icon: SiTypescript, skill: "TypeScript" },
+  { id: 3, Icon: SiReact, skill: "React" },
+  { id: 4, Icon: SiSass, skill: "Sass" },
+  { id: 5, Icon: SiSwiper, skill: "Swiper" },
+  { id: 6, Icon: SiVercel, skill: "Vercel" },
+];
+
+export const iconsKnowledgeContainer = [
+  { id: 1, Icon: SiHtml5, skill: "Html 5" },
+  { id: 2, Icon: SiCss3, skill: "Css 3" },
+  { id: 3, Icon: SiJavascript, skill: "JavaScript" },
+  { id: 4, Icon: SiNextdotjs, skill: "NextJS" },
+  { id: 5, Icon: SiReact, skill: "React" },
+  { id: 6, Icon: SiTypescript, skill: "TypeScript" },
+  { id: 7, Icon: SiSass, skill: "Sass" },
+  { id: 8, Icon: SiStyledcomponents, skill: "Styled-Components" },
+  { id: 9, Icon: SiTailwindcss, skill: "Tailwind" },
+];
+
+export const iconsKnowledgeContainerTwo = [
+  { id: 1, Icon: SiBootstrap, skill: "Bootstrap" },
+  { id: 2, Icon: SiGit, skill: "Git" },
+  { id: 3, Icon: SiGithub, skill: "GitHub" },
+  { id: 4, Icon: SiNodedotjs, skill: "NodeJS" },
+  { id: 5, Icon: SiMongodb, skill: "MongoDB" },
+  { id: 6, Icon: SiMysql, skill: "MySQL" },
+  { id: 7, Icon: SiExpress, skill: "Express" },
+  { id: 8, Icon: SiVercel, skill: "Vercel" },
+  { id: 9, Icon: SiAmazonaws, skill: "AWS" },
+];
