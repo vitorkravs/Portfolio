fcdfea33ef7cde1a5390fd8ea866b4fe99e5385a
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-09 23:14:04 -0300

    feat:  adicionando mais um padrão opcional para receber, e colocando mais uma tag

--- src/components/TitleContainer/TitleContainer.tsx
+++ src/components/TitleContainer/TitleContainer.tsx
@@ -3,12 +3,14 @@ import "./styles.scss";
 
 type titleContainerProps = {
   text: string;
+  description?: string;
 };
 
-const TitleContainer = ({ text }: titleContainerProps) => {
+const TitleContainer = ({ text, description }: titleContainerProps) => {
   return (
     <div id="title-container">
       <h2>{text}</h2>
+      {description && <p>{description}</p>}
       <span className="border-title"></span>
     </div>
   );
