3009a39ca5a5f1135233757d0e7e8f609b96c5e7
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-11 15:56:01 -0300

    refactor: atualizei a importação do componente SectionProjects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Knowledge from "@/components/Knowledge/Knowledge";
-import Projects from "@/components/Projects/Projects";
 import ScrollToTopButton from "@/components/ScrollToTopButton/ScrollToTopButton";
 import { register } from "swiper/element/bundle";
+import SectionProjects from "@/components/SectionProjects/SectionProjects";
 register();
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
       <main>
         <AboutMe />
-        <Projects />
+        <SectionProjects />
         <Knowledge />
         <Contact />
       </main>
